4f9a9f30d16b82b273887ff43c71a025e31e9387
davidepelliciari <davide.pelliciari@studio.unibo.it>
2020-11-21 19:08:30 +0100

    added Class that do posterior combination

---
+++ CombinedPosterior.h
@@ -0,0 +1,584 @@
+/********************************************************************
+ *  Copyright (C) 2014 by Federico Marulli and Alfonso Veropalumbo  *
+ *  federico.marulli3@unibo.it                                      *
+ *                                                                  *
+ *  This program is free software; you can redistribute it and/or   *
+ *  modify it under the terms of the GNU General Public License as  *
+ *  published by the Free Software Foundation; either version 2 of  *
+ *  the License, or (at your option) any later version.             *
+ *                                                                  *
+ *  This program is distributed in the hope that it will be useful, *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of  *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the   *
+ *  GNU General Public License for more details.                    *
+ *                                                                  *
+ *  You should have received a copy of the GNU General Public       *
+ *  License along with this program; if not, write to the Free      *
+ *  Software Foundation, Inc.,                                      *
+ *  59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.       *
+ ********************************************************************/
+
+/**
+ *  @file Headers/CombinedPosterior.h
+ *
+ *  @brief The class CombinedPosterior
+ *
+ *  This file defines the interface of the class CombinedPosterior
+ *
+ *  @authors Davide Pelliciari
+ *
+ *  @authors davide.pelliciari@studio.unibo.it
+ */
+
+ #ifndef __COMBINEDPOSTERIOR__
+ #define __COMBINEDPOSTERIOR__
+
+ #include "RandomNumbers.h"
+ #include "Prior.h"
+ #include "Likelihood.h"
+ #include "Posterior.h"
+
+
+ // ===================================================================================================
+
+
+namespace cbl {
+
+  namespace statistics {
+
+    /**
+    *  @class CombinedPosterior CombinedPosterior.h "Headers/CombinedPosterior.h"
+    *
+    *  @brief The class CombinedPosterior
+    *
+    *  This class is used to define the distribution
+    */
+   class CombinedPosterior : public Posterior {
+
+     private:
+
+     /// shared pointers vector containing Posterior objects
+     std::vector<std::shared_ptr<Posterior>> m_posteriors;
+
+     /// number of posteriors
+     int m_Nposteriors;
+
+     /// the prior distributions
+   	 std::vector<std::shared_ptr<cbl::statistics::Prior>> m_priors;
+
+     /// the loglikelihood distributions
+     std::vector<std::vector<double>> m_log_likelihoods;
+
+     /// likelihood inputs
+     std::vector<std::shared_ptr<void>> m_likelihood_inputs;
+
+     /// log-likelihood functions
+     std::vector<LogLikelihood_function> m_log_likelihood_functions;
+
+     /// likelihood functions
+     std::vector<Likelihood_function> m_likelihood_functions;
+
+     /// likelihood functions on a grid
+     std::vector<LogLikelihood_function> m_likelihood_functions_grid;
+
+     /// log-likelihood functions on a grid
+     std::vector<LogLikelihood_function> m_log_likelihood_functions_grid;
+
+     /// data containers
+     std::vector<std::shared_ptr<data::Data>> m_datasets;
+
+     /// model to test
+     std::vector<std::shared_ptr<Model>> m_models;
+
+     /// use_grid
+     std::vector<bool> m_use_grid;
+
+     /// vector of seeds
+     std::vector<int> m_seeds;
+
+     /// model parameters
+     std::vector<std::shared_ptr<ModelParameters>> m_model_Parameters;
+
+
+     public:
+
+  /**
+  *  @name Constructors/destructors
+  */
+  ///@{
+
+  /**
+  *  @brief default constructor
+  *
+  *
+  */
+  CombinedPosterior () = default;
+
+  /**
+	 *  @brief constructor
+	 *
+	 *  @param posteriorA
+	 *
+   *  @param posteriorB
+   *
+	 *  @param usecols
+	 *
+	 *  @param cut_sigma
+	 *
+   *  @param distNum
+   *
+   *  @param cell_size
+   *
+   *  @param rMAX
+   */
+  CombinedPosterior (const std::vector<std::shared_ptr<Posterior>> posteriors);
+
+  /**
+	 *  @brief default destructor
+	 *
+	 *
+	 */
+	~CombinedPosterior () = default;
+
+	///@}
+
+
+  /**
+   * @brief set the interal values of m_log_posterior as the concatenation
+   * of the logposterior vectors of two cosmological chains
+   *
+   * @param posteriorA the first logposterior distribution
+   *
+   * @param posteriorB the second logposterior distribution
+   *
+   */
+  void set_log_posterior(const std::vector<double> logpostA, const std::vector<double> logpostB);
+
+  /**
+   * @brief set the interal values of m_parameters as the concatenation
+   * of the parameters vectors of two cosmological chains
+   *
+   * @param posteriorA the first parameter vector
+   *
+   * @param posteriorB the second parameter vector
+   *
+   */
+  void set_parameters(const std::vector<std::vector<double>> parametersA, const std::vector<std::vector<double>> parametersB);
+
+  /**
+   * @brief set all the interal variables needed to combined Posterior objects that are
+   * constructed from chains (not external)
+   *
+   */
+  void set_all();
+
+  /**
+   * @brief do the importance sampling for two Posterior objects
+   *
+   * @param distNum number of points for the averaging
+   *
+   * @param cell_size cell dimension
+   *
+   * @param rMAX searching radius
+   *
+   * @param cut_sigma for the weight's distribution cut
+   *
+   */
+  void importance_sampling(const int distNum, const double cell_size, const double rMAX, const double cut_sigma);
+
+
+
+  /**
+   * @brief write parameters, logposteriors and weights to an output file
+   *
+   * @param output_dir the output file path
+   *
+   * @param output_file the ouput file name
+   *
+   */
+  void write_chains(const std::string output_dir, const std::string output_file);
+
+  /**
+	 * @brief initialize the chains by drawing from the prior
+	 * distributions
+	 *
+	 * the starting values of the chain are extracted from the
+	 * (possibly different) distributions of the priors
+	 *
+	 * @param chain_size the chain lenght
+	 *
+	 * @param n_walkers the number of parallel
+	 * chains
+	 *
+	 *
+	 */
+	void initialize_chains (const int chain_size, const int n_walkers);
+
+	/**
+	 * @brief initialize the chains in a ball around the posterior
+	 * best-fit parameter values
+	 *
+	 * the starting values of the chain are extracted from uniform
+	 * distributions in the range [parameter-radius,
+	 * parameter+radius] (for each likelihood parameter)
+	 *
+	 * this function first maximizes the posterior, starting the
+	 * computation at the values of the input vector 'start', then
+	 * it inizializes the chain
+	 *
+	 * @param chain_size the chain lenght
+	 *
+	 * @param n_walkers the number of parallel
+	 * chains
+	 *
+	 * @param radius radius of the ball in parameter space
+	 *
+	 * @param start std::vector containing initial values for
+	 * the posterior maximization
+	 *
+	 * @param max_iter the maximum number of iterations
+	 *
+	 * @param tol the tolerance in finding convergence
+	 *
+	 * @param epsilon the simplex side
+	 *
+	 *
+	 */
+	void initialize_chains (const int chain_size, const int n_walkers, const double radius, const std::vector<double> start, const unsigned int max_iter=10000, const double tol=1.e-6, const double epsilon=1.e-3);
+
+	/**
+	 * @brief initialize the chains in a ball around the input
+	 * parameter values
+	 *
+	 * the starting values of the chain are extracted from uniform
+	 * distributions in the range [value[i]-radius,
+	 * value[i]+radius] (for each i-th likelihood parameter)
+	 *
+	 * @param chain_size the chain lenght
+	 *
+	 * @param n_walkers the number of parallel
+	 * chains
+	 *
+	 * @param value vector containing the input values, centres of
+	 * the ball in the parameter space
+	 *
+	 * @param radius radius of the ball in the parameter space
+	 *
+	 *
+	 */
+	void initialize_chains (const int chain_size, const int n_walkers, std::vector<double> &value, const double radius);
+
+	/**
+	 * @brief initialize the chains with input values
+	 *
+	 * the starting values of the chain are the elements of the
+	 * input matrix 'chain_values'
+	 *
+	 * @param chain_size the chain lenght
+	 *
+	 * @param chain_value matrix of size (n_walkers,
+	 * n_parameters), starting values of the chain
+	 *
+	 *
+	 */
+	void initialize_chains (const int chain_size, const std::vector<std::vector<double>> chain_value);
+
+	/**
+	 * @brief initialize the chains reading from an input file
+	 *
+	 * the starting values of the chain are get from the last
+	 * lines of an input chain file; it can be used to continue an
+	 * MCMC sampling computation
+	 *
+	 * @param chain_size the chain lenght
+	 *
+	 * @param n_walkers the number of parallel
+	 * chains
+	 *
+	 * @param input_dir the input directory
+	 *
+	 * @param input_file the input file
+	 *
+	 *
+	 */
+	void initialize_chains (const int chain_size, const int n_walkers, const std::string input_dir, const std::string input_file);
+
+  /**
+   *  @brief sample the posterior using the stretch-move sampler
+   *  (Foreman-Mackey et al. 2012)
+   *
+   *  @param aa the parameter of the \f$g(z)\f$ distribution
+   *
+   *  @param parallel false \f$\rightarrow\f$ non-parallel
+   *  sampler; true \f$\rightarrow\f$ parallel sampler
+   *
+   *  @param outputFile output file where the chains are written
+         *  during run-time. Leave it to default value to have no
+         *  output.  WARNING: this option is intended for debug. It
+         *  only works for the non-parallelized stretch-move
+         *  algorithm.  The chain in output will be written in a
+         *  different format with respect to the method
+         *  cbl::statistics::Posterior::write_chain::ascii col1) chain
+         *  step col2) walker index col3-npar) parameter values col
+         *  npar+3) value of the posterior
+   *
+   *  @param start the minimum chain position used to compute
+   *  the median
+   *
+   *  @param thin the step used for chain dilution
+   *
+   *  @param nbins the number of bins to estimate the posterior
+   *  distribution, used to assess its properties
+   *
+   *
+   *
+   *  @warning if parallel is set true, than pointers cannot be
+   *  used inside the posterior function
+   */
+   void sample_stretch_move (const double aa=2, const bool parallel=true, const std::string outputFile=par::defaultString, const int start=0, const int thin=1, const int nbins=50);
+
+
+  /**
+	 *  @brief evaluate the un-normalized posterior as the product of the
+   *  un-normalized entry posteriors
+	 *
+	 *  \f[ P((\vec{\theta} | \vec{d}) =
+	 *  \mathcal{L}(\vec{d}|\vec{\theta}) \cdot Pr(\vec{\theta})
+	 *  \f]
+	 *
+	 *  where \f$P\f$ is the
+	 *  posterior,\f$\mathcal{L}(\vec{d}|\vec{\theta})\f$ is the
+	 *  likelihood and \f$Pr(\vec{\theta})\f$ is the prior
+	 *
+	 *  @param pp the parameters
+	 *
+	 *  @return the value of the un-normalized posterior
+	 */
+  double operator () (std::vector<double> &pp) const;
+
+  /**
+   *  @brief evaluate the logarithm of the un-normalized
+   *  posterior as the sum of all the logarithm of the un-nurmalized
+   *  posteriors of the input Posterior objects
+   *
+   *  formule da cambiare
+   *
+   *  \f[ P(\vec{\theta} | \vec{d}) =
+   *  \mathcal{L}(\vec{d}|\vec{\theta}) \cdot Pr(\vec{\theta})
+   *  \f]
+   *
+   *  where \f$P\f$ is the
+   *  posterior,\f$\mathcal{L}(\vec{d}|\vec{\theta})\f$ is the
+   *  likelihood and \f$Pr(\vec{\theta})\f$ is the prior
+   *
+   *  @param pp the parameters
+   *
+   *  @return the logarithm of the un-normalized posterior
+   */
+  double log (std::vector<double> &pp) const;
+
+  /**
+   *  @brief function that maximizes the posterior, finds the
+   *  best-fit parameters and store them in the model
+   *
+   *  this function exploits the Nelder-Mead method
+   *  https://en.wikipedia.org/wiki/Nelder%E2%80%93Mead_method
+   *
+   *  the algorithm defines a simplex (i.e a k-dimensional
+   *  polytope which is the convex hull of its k+1 vertices) in
+   *  the parameter space. At each step, it identifies the
+   *  simplex vertex at which the function to be minimised
+   *  (i.e. the negative posterior in this case) has the
+   *  greatest value, and moves it, via reflections and scaling,
+   *  to a new position in which the function has a lower
+   *  value. This iteration stops when the simplex area becomes
+   *  lower than the tolerance. For instance, in 2D, the
+   *  starting vertices of the simplex (a triangle in 2D) are
+   *  the following: (start[0], start[1]) ; (start[0]+epsilon,
+   *  start[1]) ; (start[0], start[1]+epsilon)
+   *
+   *  @param start std::vector containing initial values for the
+   *  posterior maximization
+   *
+   *  @param parameter_limits limits for the parameters
+   *
+   *  @param max_iter the maximum number of iterations
+   *
+   *  @param tol the tolerance in finding convergence
+   *
+   *  @param epsilon the simplex side
+   *
+   *
+   */
+  void maximize (const std::vector<double> start, const std::vector<std::vector<double>> parameter_limits, const unsigned int max_iter=10000, const double tol=1.e-6, const double epsilon=1.e-3)
+  { (void)start; (void)parameter_limits; (void)max_iter; (void)tol; (void)epsilon; ErrorCBL("the method is used without parameter_ranges!", "maximize", "Posterior.h"); }
+
+  /**
+   *  @brief function that maximize the posterior, find the
+   *  best-fit parameters and store them in model
+   *
+   *  @param start std::vector containing initial values for
+   *  the posterior maximization
+   *
+   *  @param max_iter the maximum number of iterations
+   *
+   *  @param tol the tolerance in finding convergence
+   *
+   *  @param epsilon the simplex side
+   *
+   *
+   */
+  void maximize (const std::vector<double> start, const unsigned int max_iter=10000, const double tol=1.e-6, const double epsilon=1.e-4);
+
+  /**
+	 * @brief store the results of the MCMC sampling to file
+	 *
+	 * this function stores to file the posterior mean, the
+	 * posterior standard deviation, the posterior median, 18th
+	 * and 82th posterior percentiles, and, optionally, the
+	 * posterior mode.
+	 *
+	 * If the covariance matrix has been estimated from a set of
+	 * mock catalogues, and the input parameters ns (number of
+	 * samples used to estimate the covariance matrix) and nb
+	 * (number of data measurements, e.g. the bins of the dataset)
+	 * are provided (>0), then the parameter errors
+	 * (\f$\sigma_p\f$) will be corrected to take into account the
+	 * uncertainities in the covariance estimate (Percival et
+	 * al. 2014):
+	 *
+	 * \f[ \sigma_p = \sqrt{\frac{1+B(n_b-n_p)}{1+A+B(n_p+1)}} \f]
+	 *
+	 * where
+	 *
+	 * \f[ A = \frac{2}{(n_s-n_b-1)(n_s-n_b-4)} \,, \f]
+	 *
+	 * \f[ B = \frac{(n_s-n_b-2)}{(n_s-n_b-1)(n_s-n_b-4)} \,. \f]
+	 *
+	 * this correction can be applied only if the likelihood is
+	 * Gaussian. Morever, the inverce covariance matrix estimator
+	 * has to be corrected to take into account the inverse
+	 * Wishart distribution (Hartlap, Simon and Schneider 2006).
+	 *
+	 * @param output_dir the output directory
+	 *
+	 * @param root_file the root of the output file to be written
+	 *
+	 * @param start the minimum chain position to be written
+	 *
+	 * @param thin the step used for dilution on screen
+	 *
+	 * @param nbins the number of bins to estimate the posterior
+	 * distribution, used to assess its properties
+	 *
+	 * @param fits false \f$\rightarrow\f$ ascii file; true
+	 * \f$\rightarrow\f$ fits file
+	 *
+	 * @param compute_mode true \f$\rightarrow\f$ compute the
+	 * posterior mode; false \f$\rightarrow\f$ do not compute the
+	 * posterior mode
+	 *
+	 * @param ns number of samples used to estimate the covariance
+	 * matrix
+	 *
+	 * @param nb number of data measurements, e.g. the bins of the
+	 * dataset
+	 *
+	 *
+	 */
+  void write_results (const std::string output_dir, const std::string root_file, const int start=0, const int thin=1, const int nbins=50, const bool fits=false, const bool compute_mode=false, const int ns=-1, const int nb=-1);
+
+  /**
+   * @brief write the chains obtained after
+   * the MCMC sampling
+   *
+   * @param output_dir the output directory
+   *
+   * @param output_file the output file
+   *
+   * @param start the minimum chain position to be written
+   *
+   * @param thin the step used for dilution
+   *
+   * @param is_FITS_format true \f$\rightarrow\f$ the format of
+   * the input file is FITS; false \f$\rightarrow\f$ the format
+   * of the input file is ASCII
+   *
+   * @param prec decimal precision
+   *
+   * @param ww number of characters to be used as field width
+   *
+   * @warning column only work for ascii chain file
+   *
+   *
+   */
+  void write_chain (const std::string output_dir, const std::string output_file, const int start=0, const int thin=1, const bool is_FITS_format=false, const int prec=5, const int ww=14);
+
+  /**
+   * @brief write the chains obtained after the MCMC sampling on
+   * an ascii file
+   *
+   * @param output_dir the output directory
+   *
+   * @param output_file the output file
+   *
+   * @param start the minimum chain position to be written
+   *
+   * @param thin the step used for dilution
+   *
+   * @param prec decimal precision
+   *
+   * @param ww number of characters to be used as field width
+   *
+   *
+   */
+  void write_chain_ascii (const std::string output_dir, const std::string output_file, const int start=0, const int thin=1, const int prec=5, const int ww=14);
+
+  /**
+   * @brief write the chains obtained after the MCMC sampling on
+   * a FITS file
+   *
+   * @param output_dir the output directory
+   *
+   * @param output_file the output file
+   *
+   * @param start the minimum chain position to be written
+   *
+   * @param thin the step used for dilution
+   *
+   *
+   */
+  void write_chain_fits (const std::string output_dir, const std::string output_file, const int start=0, const int thin=1);
+
+  /**
+	 * @brief write the model computing 16th, 50th and
+	 * 84th percentiles from the MCMC chains
+	 *
+	 * @param output_dir the output directory
+	 *
+	 * @param output_file the output file
+	 *
+	 * @param start the minimum chain position to be written
+	 *
+	 * @param thin the step used for dilution on screen
+	 *
+	 *
+	 */
+  void write_model_from_chain (const std::string output_dir, const std::string output_file, const int start, const int thin);
+
+  /**
+   * @brief write maximization results on a file
+   *
+   * @param output_dir the output directory
+   *
+   * @param root_file the root of the output file to be written
+   *
+   *
+   */
+  void write_maximization_results (const std::string dir_output, const std::string file);
+
+    };
+  }
+}
+
+#endif
