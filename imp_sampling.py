036e3c75150fe21c453222b00b3e92d62fc272f8
davidepelliciari <davide.pelliciari@studio.unibo.it>
2020-11-21 19:09:07 +0100

    update im_sampling python code

---
+++ imp_sampling.py
@@ -0,0 +1,22 @@
+from scipy.spatial import Voronoi
+from scipy.spatial import ConvexHull
+from shapely.geometry import Polygon
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import scipy.interpolate
+import scipy.spatial
+from zipfile import ZipFile
+import time
+!pip install corner
+import corner
+
+# https://docs.python.org/3/library/sys.html
+import sys
+
+# https://docs.python.org/3/library/urllib.html
+import urllib.request as request
+from urllib.request import urlopen
+
+# https://docs.python.org/3/library/io.html
+from io import BytesIO
